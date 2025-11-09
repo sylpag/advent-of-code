bc37e69f106b24da3f31cac96c1a882133e69e2b
sylpag <sylvain@letspowerhouse.com>
2025-11-09 20:42:54 +0100

    2024 day 1 solution w/ part 2

--- 2024-day1.py
+++ 2024-day1.py
@@ -1,6 +1,8 @@
 # IMPORTS
 import pandas as pd
 import numpy as np
+
+# PART 1
 # INITIALIZATION
 # Get input data
 input_data = pd.read_csv('data/2024-day1', sep=r'\s+', header=None)
@@ -20,3 +22,18 @@ pairwise_diff = abs(array1_sorted - array2_sorted)
 # Sum distances
 array_diff = np.sum(pairwise_diff)
 print("Array difference:", array_diff)
+
+# PART 2
+# Count the number of times each unique number in array 2 appears
+unique_vals_2, unique_counts_2 = np.unique(array2, return_counts=True)
+
+# Map values to their counts
+array_dict = dict(zip(unique_vals_2, unique_counts_2))
+
+# Use dict 1 as vector function to count how often vals in array 1 appear in array 2
+array1_counts = np.vectorize(array_dict.get)(array1, 0)
+
+# Calculate similarity score element by element
+similarity_score = np.dot(array1, array1_counts)
+
+print("Total similarity score:", similarity_score)
